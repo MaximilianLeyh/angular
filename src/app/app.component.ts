696571bff9866f59b9b3da7016ee6657d3abf491
MaximilianLeyh <118059860+MaximilianLeyh@users.noreply.github.com>
2023-02-19 14:31:13 +0100

    started header menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'maximilianleyh';
+export class AppComponent  {
+  
+
 }
